e559f074b88c550303a6ca252b2353d2ef7188f3
DESKTOP-0V9VAH6\ashok <ashok.nagarajan@tenpo.cl>
2021-05-02 22:32:03 +0530

    DB config changed to clever cloud

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername='localhost';
-$username='root';
-$password='root';
-$dbname = "student";
+$servername='bcj1nlpdqi8j7sjsipbx-mysql.services.clever-cloud.com';
+$username='ucfrgh7p8hufd5km';
+$password='7aQFuqO0iAqMje7EM53u';
+$dbname = "bcj1nlpdqi8j7sjsipbx";
 $conn = mysqli_connect($servername,$username,$password,$dbname);
   if(!$conn){
       die('Could not Connect MySql Server:' .mysqli_connect_error($conn));
